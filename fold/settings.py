aeb182aab727f18f5c7b8e191804064dcdbb7cf1
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-06-23 22:40:50 +0100

    Adding extra product images to the media directory

--- fold/settings.py
+++ fold/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%7sxox!(n#qp3vmi#4coijpj-gb1-mxb(*g$^op&5#7fxivos%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
