457b537d5f115c54d88d19eb81b7b9f5c73d1401
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-07-10 12:36:26 +0100

    Adjusting AWS configuration in Settings.py

--- fold/settings.py
+++ fold/settings.py
@@ -197,6 +197,8 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
