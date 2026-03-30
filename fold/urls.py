d4ba588bbe82b129face83782cf149e00152be6e
maggku <maggie.kuczynska@gmail.com>
2026-03-30 21:44:46 +0100

    Add apps. path to apps in the root urls.py

--- fold/urls.py
+++ fold/urls.py
@@ -21,9 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
-    path('products/', include('products.urls')),
-    path('bag/', include('bag.urls')),
-    path('checkout/', include('checkout.urls')),
-    path('profile/', include('profiles.urls')),
+    path('', include('apps.home.urls')),
+    path('products/', include('apps.products.urls')),
+    path('bag/', include('apps.bag.urls')),
+    path('checkout/', include('apps.checkout.urls')),
+    path('profile/', include('apps.profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
