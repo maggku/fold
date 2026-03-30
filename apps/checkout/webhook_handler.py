eda04b79c4d725e978e12d5e76c351c0e9e9bc6a
maggku <maggie.kuczynska@gmail.com>
2026-03-30 23:08:45 +0100

    Change import path in checkout/webhook_handler.py

--- apps/checkout/webhook_handler.py
+++ apps/checkout/webhook_handler.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 from .models import Order, OrderLineItem
 from apps.products.models import Product
-from apps.profiles import UserProfile
+from apps.profiles.models import UserProfile
 
 import json
 import time
